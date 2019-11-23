9ad869fa1361f2fd6918ee205e5bc7701f72a11a
Ko-ta Shingaki <e195711@ie.u-ryukyu.ac.jp>
2019-11-23 19:55:06 +0900

    complete step2

--- src/main/jp/ac/uryukyu/ie/e195711/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e195711/Enemy.java
@@ -1,63 +1,32 @@
 package jp.ac.uryukyu.ie.e195711;
 
 /**
- * 敵クラス。
+ * 生物クラスを継承して敵クラスを定義する。
+ * 定義しているフィールド変数は以下。
  *  String name; //敵の名前
  *  int hitPoint; //敵のHP
  *  int attack; //敵の攻撃力
  *  boolean dead; //敵の生死状態。true=死亡。
- * Created by tnal on 2016/11/13.
+ * Created by ko-ta on 2019/11/23.
  */
-public class Enemy {
-    String name;
-    int hitPoint;
-    int attack;
-    boolean dead;
-
+public class Enemy extends LivingThing{
     /**
-     * コンストラクタ。名前、最大HP、攻撃力を指定する。
+     * superクラスのコントラクタを呼び出す。
+     * 名前、最大HP、攻撃力を指定する。
      * @param name モンスター名
      * @param maximumHP モンスターのHP
      * @param attack モンスターの攻撃力
      */
     public Enemy (String name, int maximumHP, int attack) {
-        this.name = name;
-        hitPoint = maximumHP;
-        this.attack = attack;
-        dead = false;
-        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
-    }
-
-    /**
-     * getterメソッドと同等。生死をboolean表現しているためメソッド名をisDead()とした。
-     * @return boolean
-     */
-    public boolean isDead() {
-        return dead;
-    }
-
-    public String getName(){
-        return name;
-    }
-
-    /**
-     * Heroへ攻撃するメソッド。
-     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
-     * @param hero 攻撃対象
-     */
-    public void attack(Hero hero){
-        if (!isDead()) {
-            int damage = (int) (Math.random() * attack);
-            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
-            hero.wounded(damage);
-        }
+        super(name, maximumHP, attack);
     }
 
     /**
      * 自身へ攻撃されたときのダメージ処理をするメソッド。
-     * 指定されたダメージを hitPoint から引き、死亡判定を行う。
+     * superクラスと処理後の出力メッセージが異なる。
      * @param damage 受けたダメージ
      */
+    @Override
     public void wounded(int damage){
         hitPoint -= damage;
         if( hitPoint < 0 ) {
--- src/main/jp/ac/uryukyu/ie/e195711/Hero.java
+++ src/main/jp/ac/uryukyu/ie/e195711/Hero.java
@@ -1,61 +1,32 @@
 package jp.ac.uryukyu.ie.e195711;
 
 /**
- * ヒーロークラス。
- *  String name; //敵の名前
- *  int hitPoint; //敵のHP
- *  int attack; //敵の攻撃力
- *  boolean dead; //敵の生死状態。true=死亡。
- * Created by tnal on 2016/11/13.
+ * 生物クラスを継承してヒーロークラスを定義する。
+ * 定義しているフィールド変数は以下。
+ *  String name; //ヒーローの名前
+ *  int hitPoint; //ヒーローのHP
+ *  int attack; //ヒーローの攻撃力
+ *  boolean dead; //ヒーローの生死状態。true=死亡。
+ * Created by ko-ta on 2019/11/23.
  */
-public class Hero {
-    String name;
-    int hitPoint;
-    int attack;
-    boolean dead;
-
+public class Hero extends LivingThing{
     /**
-     * コンストラクタ。名前、最大HP、攻撃力を指定する。
-     * @param name ヒーロー名
+     * superクラスのコントラクタを呼び出す。
+     * 名前、最大HP、攻撃力を指定する。
+     * @param name ヒーローの名前
      * @param maximumHP ヒーローのHP
      * @param attack ヒーローの攻撃力
      */
-    public Hero (String name, int maximumHP, int attack) {
-        this.name = name;
-        hitPoint = maximumHP;
-        this.attack = attack;
-        dead = false;
-        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
-    }
-
-    /**
-     * getterメソッドと同等。生死をboolean表現しているためメソッド名をisDead()とした。
-     * @return boolean
-     */
-    public boolean isDead(){
-        return  dead;
-    }
-
-    public String getName(){
-        return name;
-    }
-
-    /**
-     * Enemyへ攻撃するメソッド。
-     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
-     * @param e 攻撃対象
-     */
-    public void attack(Enemy e){
-        int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, e.getName(), damage);
-        e.wounded(damage);
+    public Hero(String name, int maximumHP, int attack){
+        super(name, maximumHP, attack);
     }
 
     /**
      * 自身へ攻撃されたときのダメージ処理をするメソッド。
-     * 指定されたダメージを hitPoint から引き、死亡判定を行う。
+     * superクラスと処理後の出力メッセージが異なる。
      * @param damage 受けたダメージ
      */
+    @Override
     public void wounded(int damage){
         hitPoint -= damage;
         if( hitPoint < 0 ) {
